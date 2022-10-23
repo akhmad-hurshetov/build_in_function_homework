a2fea987c625f5f004b017368986cd0f42b0d944
Akhmad <akhmadhurshetov@gmail.com>
2022-10-24 02:26:54 +0500

    04

--- build_func04.py
+++ build_func04.py
@@ -1,5 +1,5 @@
 def main(n):
-    n = 8 * (7 - 4)**(4 - 2)
+    n = 8 * (n - 4)**(4 - 2)
     """
     Given a argument called 'n' type of int , calculate the value of expression and return result:
     Args:
